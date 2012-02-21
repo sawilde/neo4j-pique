ae265f388cd4568d86a1941942399d8f2cf9b392
Shaun Wilde <shaun.wilde@commonvision.com.au>
2012-02-21 22:36:54 +1100

    my first rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neo4jPique::Application.config.secret_token = '3c080f55a36e38882abd80e040421f4180eb2d3fb2acb0301dbb05b3b584ac0c3a61d0a5b7666a3302e8e6cf1d0ddff664c45debed0487d9d433b6cd66a2cca2'
