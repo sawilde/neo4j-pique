9f48318f7a6f21abebe0d43cf059e48be844a7ed
Shaun Wilde <shaun.wilde@commonvision.com.au>
2012-02-23 12:05:05 +1100

    add simple controller - with twitter login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Neo4jPique::Application.routes.draw do
+  get "main/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ Neo4jPique::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'main#index'
 
   # See how all your routes lay out with "rake routes"
 
